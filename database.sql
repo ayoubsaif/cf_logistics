e1528d9f7ce5b02936828f240cbd45550c69e314
Ayoub <34210464+ayoubsaif@users.noreply.github.com>
2023-07-10 00:04:14 +0200

    add new changes in database structure

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-07-2023 a las 23:39:28
+-- Tiempo de generación: 10-07-2023 a las 00:03:23
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -29,39 +29,43 @@ USE `cr_logistics_dev`;
 -- Estructura de tabla para la tabla `accounts`
 --
 
+DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
+  `accountId` varchar(36) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `status` tinyint(4) NOT NULL,
   `companyName` varchar(255) DEFAULT NULL,
   `companyLegalName` varchar(255) DEFAULT NULL,
-  `companyVatNo` varchar(20) DEFAULT NULL
+  `companyVat` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `name`, `status`, `companyName`, `companyLegalName`, `companyVatNo`) VALUES
-(1, 'Trendico', 1, 'Tréndico Group SL', 'Trendico Group S.L.', 'B99021644'),
-(2, 'Oxigeno', 1, 'Oxigeno Iberspor SL', 'Oxigeno Iberspor SL', 'B99350621');
+INSERT INTO `accounts` (`id`, `accountId`, `name`, `status`, `companyName`, `companyLegalName`, `companyVat`) VALUES
+(1, '', 'Trendico', 1, 'Tréndico Group SL', 'Trendico Group S.L.', 'B99021644'),
+(2, '', 'Oxigeno', 1, 'Oxigeno Iberspor SL', 'Oxigeno Iberspor SL', 'B99350621'),
+(657, '3e98a348-1db0-11ee-8349-bf4d446d586f', 'crasforum', 1, 'Cras Forum', 'GESTION DIGITAL LABS 23 SL', 'B02668911');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `accountsusersrel`
+-- Estructura de tabla para la tabla `accounts_users_rel`
 --
 
-CREATE TABLE `accountsusersrel` (
+DROP TABLE IF EXISTS `accounts_users_rel`;
+CREATE TABLE `accounts_users_rel` (
   `userId` int(11) NOT NULL,
   `accountId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `accountsusersrel`
+-- Volcado de datos para la tabla `accounts_users_rel`
 --
 
-INSERT INTO `accountsusersrel` (`userId`, `accountId`) VALUES
+INSERT INTO `accounts_users_rel` (`userId`, `accountId`) VALUES
 (1, 1),
 (1, 2);
 
@@ -71,12 +75,13 @@ INSERT INTO `accountsusersrel` (`userId`, `accountId`) VALUES
 -- Estructura de tabla para la tabla `addresses`
 --
 
+DROP TABLE IF EXISTS `addresses`;
 CREATE TABLE `addresses` (
   `id` int(20) NOT NULL,
   `addressName` varchar(255) DEFAULT NULL,
   `contactName` varchar(255) DEFAULT NULL,
   `companyName` varchar(255) DEFAULT NULL,
-  `docId` varchar(20) DEFAULT NULL,
+  `vat` varchar(20) DEFAULT NULL,
   `street` varchar(255) DEFAULT NULL,
   `streetComplement` varchar(255) DEFAULT NULL,
   `postalCode` varchar(20) DEFAULT NULL,
@@ -95,6 +100,7 @@ CREATE TABLE `addresses` (
 -- Estructura de tabla para la tabla `site_config`
 --
 
+DROP TABLE IF EXISTS `site_config`;
 CREATE TABLE `site_config` (
   `variable` varchar(50) NOT NULL,
   `value` varchar(255) DEFAULT NULL
@@ -113,13 +119,32 @@ INSERT INTO `site_config` (`variable`, `value`) VALUES
 -- Estructura de tabla para la tabla `stores`
 --
 
+DROP TABLE IF EXISTS `stores`;
 CREATE TABLE `stores` (
   `id` int(11) NOT NULL,
   `storeId` bigint(13) NOT NULL,
-  `accountId` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `commercialName` varchar(255) DEFAULT NULL
+  `accountId` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `commercialName` varchar(255) DEFAULT NULL,
+  `contactName` varchar(255) DEFAULT NULL,
+  `vat` varchar(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `streetComplement` varchar(255) DEFAULT NULL,
+  `postalCode` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `contactPhone` varchar(255) DEFAULT NULL,
+  `contactMobile` varchar(255) DEFAULT NULL,
+  `contactEmail` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `stores`
+--
+
+INSERT INTO `stores` (`id`, `storeId`, `accountId`, `name`, `commercialName`, `contactName`, `vat`, `street`, `streetComplement`, `postalCode`, `city`, `state`, `country`, `contactPhone`, `contactMobile`, `contactEmail`) VALUES
+(0, 1, 1, 'Foot on Mars Store Zaragoza', 'Foot on Mars Zaragoza', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -127,6 +152,7 @@ CREATE TABLE `stores` (
 -- Estructura de tabla para la tabla `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL COMMENT 'E-Mail for contact and login',
@@ -155,12 +181,12 @@ INSERT INTO `users` (`id`, `email`, `password`, `firstName`, `lastName`, `role`,
 -- Indices de la tabla `accounts`
 --
 ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`,`accountId`) USING BTREE;
 
 --
--- Indices de la tabla `accountsusersrel`
+-- Indices de la tabla `accounts_users_rel`
 --
-ALTER TABLE `accountsusersrel`
+ALTER TABLE `accounts_users_rel`
   ADD PRIMARY KEY (`userId`,`accountId`) USING BTREE,
   ADD KEY `accountId` (`accountId`);
 
@@ -182,7 +208,7 @@ ALTER TABLE `site_config`
 -- Indices de la tabla `stores`
 --
 ALTER TABLE `stores`
-  ADD PRIMARY KEY (`id`,`storeId`),
+  ADD PRIMARY KEY (`id`,`storeId`,`accountId`,`name`) USING BTREE,
   ADD KEY `Account ID` (`accountId`);
 
 --
@@ -202,7 +228,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=658;
 
 --
 -- AUTO_INCREMENT de la tabla `addresses`
@@ -221,9 +247,9 @@ ALTER TABLE `users`
 --
 
 --
--- Filtros para la tabla `accountsusersrel`
+-- Filtros para la tabla `accounts_users_rel`
 --
-ALTER TABLE `accountsusersrel`
+ALTER TABLE `accounts_users_rel`
   ADD CONSTRAINT `Account` FOREIGN KEY (`accountId`) REFERENCES `accounts` (`id`),
   ADD CONSTRAINT `User` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
 
