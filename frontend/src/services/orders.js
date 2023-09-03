9ad380d81c9ef63ca7c130541bde11b208732068
Ayoub <34210464+ayoubsaif@users.noreply.github.com>
2023-09-04 00:04:22 +0200

    change orders request to next api endpoint by security reasons

--- frontend/src/services/orders.js
+++ frontend/src/services/orders.js
@@ -1,18 +1,28 @@
 
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_APP_API_HOST;
+const API_URL = process.env.NEXT_PUBLIC_API;
 
 // get orders by account and store id
-export const getOrders = (accountId, storeId) => {
-    const headers = {
-        'accountid': accountId
-    };
-    return axios.get(`${API_URL}/api/orders/${storeId}`, { headers });
+export const getAllOrders = (accountId, storeId, accessToken, page) => {
+    try {
+        const headers = {
+            'Authorization': `Bearer ${accessToken}`,
+            'AccountId': accountId
+        };
+        const params = {
+            'page': page
+        };
+        return axios.get(`/api/orders/${storeId}`, { headers, params });
+    }
+    catch (err) {
+        console.log(err);
+    }
 };
 
-export const getOpenOrders = (accountId, storeId) => {
+export const getOpenOrders = (accountId, storeId, accessToken) => {
     const headers = {
+        'Authorization': `Bearer ${accessToken}`,
         'accountid': accountId
     };
     const params = {
