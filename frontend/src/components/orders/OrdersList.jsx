8c3e844d0a11802a41b6608f24993dd94d9473df
Ayoub <34210464+ayoubsaif@users.noreply.github.com>
2023-09-19 23:56:38 +0200

    refactor order list component to 2 different components

--- frontend/src/components/orders/OrdersList.jsx
+++ frontend/src/components/orders/OrdersList.jsx
@@ -3,29 +3,15 @@ import { SimpleGrid, Text, HStack, Spinner } from '@chakra-ui/react';
 import OrderCard from './OrderCard';
 import OrderCardSkeleton from './OrderCardSkeleton';
 
-const OrdersList = React.forwardRef(({ isLoading, orders, hasMore }, ref) => {
+const OrdersList = React.forwardRef(({ isLoading, orders }, ref) => {
     return (
-        <SimpleGrid columns={1} gap={2} alignContent="center">
-            {isLoading ? (
-                [...Array(5)].map((e, i) => <OrderCardSkeleton key={i} />)
-            ) :
-                orders && orders?.map((order, index) => (
-                    <React.Fragment key={index}>
-                        <OrderCard order={order} />
-                        {index === orders.length - 1 && hasMore && (
-                            <HStack spacing={4} py={4} m="0 auto">
-                                <Spinner
-                                    ref={...ref}
-                                    speed='0.65s'
-                                    color='brand.500'
-                                />
-                                <Text>Cargando más pedidos...</Text>
-                            </HStack>
-                        )}
-                    </React.Fragment>
-                ))
-            }
-        </SimpleGrid>
+        isLoading ? (
+            [...Array(5)].map((e, i) => <OrderCardSkeleton key={i} />)
+        ) :
+            orders && orders?.map((order, index) => (
+                <OrderCard key={index} order={order} />
+            ))
+
     )
 });
 
