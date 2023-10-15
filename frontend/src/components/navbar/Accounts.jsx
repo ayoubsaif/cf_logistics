605a5d03e76aa8d2aca2f10f7c64e550085e223a
Ayoub <34210464+ayoubsaif@users.noreply.github.com>
2023-10-15 17:40:09 +0200

    fix(navmenu)

---
+++ frontend/src/components/navbar/Accounts.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { useForm } from 'react-hook-form';
+import { useSession } from "next-auth/react";
+import {
+    MenuGroup,
+    MenuItem,
+} from "@chakra-ui/react";
+import { useQuery } from 'react-query';
+import { get } from "@/utils/RequestFactory";
+
+export default function AccountsModal(props) {
+    const Router = useRouter();
+    const { data: session, update } = useSession();
+    const { isLoading, error, data } = useQuery("accounts", () =>
+        get("/api/accounts").then((res) => res.json())
+    );
+    const [selectedAccountId, setSelectedAccountId] = useState(null);
+
+    const handleSelectChange = async (account) => {
+        await update({ ...session, user: { ...session.user, accountId: account.value, accountName: account.label } });
+        setSelectedAccountId(selectedValue);
+        setValue("account", selectedValue); // Update the form value
+        Router.push('/');
+    };
+    console.log(data);
+    return (
+        <>
+            {data && (
+                <MenuGroup {...props} title="Cuentas">
+                    {data.map((account) => {
+                        <MenuItem key={account.id}
+                            onClick={() => handleSelectChange(account)}
+                            pl={5}>
+                            {account.name}
+                        </MenuItem>
+                    })}
+                </MenuGroup>
+            )}
+        </>
+    );
+}
---
+++ frontend/src/components/navbar/ProfileMenu.jsx
@@ -0,0 +1,80 @@
+import {
+  Menu,
+  MenuButton,
+  MenuGroup,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Flex,
+  Avatar,
+  Text,
+  Box,
+  Center,
+  useDisclosure,
+  Button
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+
+import {
+  RiLogoutCircleLine as LogoutCircleIcon,
+  RiAdminLine as AdminIcon,
+  RiGroupLine as UsersIcon,
+  RiBuilding4Line as AccountsIcon,
+  RiBuildingLine as AccountIcon,
+} from "react-icons/ri";
+import Accounts from "@/components/navbar/Accounts";
+
+export default function ProfileMenu({ session, signOut }) {
+  return (
+    <Menu>
+      <MenuButton rounded={"full"} variant={"link"} cursor={"pointer"} minW={0}>
+        <Flex direction={"row"} align={"center"} p={2}>
+          <Avatar size={"sm"} src={session.user.image} />
+          <Text ml={2} display={{ base: "none", md: "flex" }}>
+            {session.user.firstname}
+          </Text>
+        </Flex>
+      </MenuButton>
+      <MenuList
+        overflow={"hidden"}
+        alignItems={"center"}
+      >
+        <Flex direction={"row"} align={"center"} p={2}>
+          <Center>
+            <Avatar size={"lg"} src={session.user.image} />
+          </Center>
+          <Box px={2} ml={2}>
+            <Text>{session.user.name}</Text>
+            <Text>{session.user.accountName}</Text>
+          </Box>
+        </Flex>
+        <MenuDivider />
+        {session.user.role === "admin" && (
+          <>
+            <MenuGroup fontSize="md" title="Admin" icon={<AdminIcon />}>
+              <MenuItem as={NextLink} href={"/admin/users"} pl={5}
+                icon={<UsersIcon size={18} />}>
+                Usuarios
+              </MenuItem>
+              <MenuItem as={NextLink} href={"/admin/accounts"} pl={5}
+                icon={<AccountsIcon size={18} />}>
+                Cuentas
+              </MenuItem>
+            </MenuGroup>
+            <MenuDivider />
+          </>
+        )}
+        <Accounts fontSize="md" />
+        <MenuDivider />
+        <MenuGroup fontSize="md">
+          <MenuItem
+            onClick={() => signOut({ callbackUrl: "/" })}
+            pl={5}
+            icon={<LogoutCircleIcon size={18} />}>
+            Cerrar sesión
+          </MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
+  );
+}
